735c790e0f430dacf4b105566832a75ec40854fe
Georgeskf <georges_kfoury_00@live.com>
2023-11-19 00:59:48 +0200

    init

--- DS_Partiel/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace DS_Partiel.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
--- DS_Partiel/Models/Joke.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace DS_Partiel.Models
-{
-    public class Joke
-    {
-        public string Name { get; set; }
-
-    }
-}
